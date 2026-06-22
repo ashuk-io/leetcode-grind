d473fbc974d598553088276cef33d70244a04fd7
Ashutosh Kumar <146306274+ashuk-io@users.noreply.github.com>
2026-06-22 19:29:52 +0530

    Time: 0 ms (100%), Space: 20.8 MB (23.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int unique = 0;
+        for ( int i : nums){
+            unique ^= i;
+        }
+        return unique;
+    }
+};
